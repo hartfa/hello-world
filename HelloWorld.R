6ca68f4204eb236708a864d9f343a02d64feee34
joshkenney <joshuagkenney@gmail.com>
2023-04-12 14:55:14 -0400

    added lessons to HelloWorld.R

--- HelloWorld.R
+++ HelloWorld.R
@@ -1,1 +1,28 @@
 print("Hello World")
+
+# great job!
+
+# let's keep it going :)
+
+# lesson 1: variable assignment
+
+# here is a refresher on how to assign a value to a variable:
+# variable <- some value
+# in this example, I am assigning the string "Grace" to the variable `name`
+# a string is a datatype which is represented by text in quotation marks (either single or double quotes)
+
+name <- "Grace"
+
+# now, the string "Grace" is stored in the variable `name` and we can access that variable elsewhere in our script
+
+# lesson 2: using built-in functions with variables
+
+# a function is a piece of reusable code which we can call with its given name and a pair of parentheses
+# the things we put inside of the parenthesis are called arguments
+
+# in your "Hello World" script, you've already passed the string "Hello World" into the print() function as print("Hello World")
+
+# we can also pass variables as arguments to the print() function
+
+# exercise 1: insated if printing "Hello World", use print() to pring the variable `name`
+
